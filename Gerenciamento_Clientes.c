890253c29bcd799e44673e60a9b47a21e083dfef
SILVA, H. N. <hudsonnsilva@gmail.com>
2026-04-23 16:10:31 -0300

    Update Gerenciamento_Clientes.c

--- Gerenciamento_Clientes.c
+++ Gerenciamento_Clientes.c
@@ -1,1 +1,61 @@
 
+#include <stdio.h>
+#include <string.h>
+
+#define MAX 100
+
+// Estrutura do cliente
+struct Cliente {
+    int id;
+    char nome[50];
+    char telefone[20];
+};
+
+int main() {
+    struct Cliente clientes[MAX];
+    int total = 0;
+    int opcao = 0;
+
+    while (opcao != 4) {
+        printf("\n===== SISTEMA DE CLIENTES =====\n");
+        printf("1 - Cadastrar cliente\n");
+        printf("2 - Listar clientes\n");
+        printf("3 - Buscar cliente\n");
+        printf("4 - Sair\n");
+        printf("Escolha uma opcao: ");
+        scanf("%d", &opcao);
+        getchar(); // limpar buffer
+
+        // CADASTRAR
+        if (opcao == 1) {
+            if (total < MAX) {
+                clientes[total].id = total + 1;
+
+                printf("Nome: ");
+                fgets(clientes[total].nome, 50, stdin);
+                clientes[total].nome[strcspn(clientes[total].nome, "\n")] = '\0';
+
+                printf("Telefone: ");
+                fgets(clientes[total].telefone, 20, stdin);
+                clientes[total].telefone[strcspn(clientes[total].telefone, "\n")] = '\0';
+
+                total++;
+                printf("Cliente cadastrado com sucesso!\n");
+            } else {
+                printf("Limite de clientes atingido!\n");
+            }
+        }
+
+
+
+
+
+        // OPÇÃO INVÁLIDA
+        else if (opcao != 4) {
+            printf("Opcao invalida!\n");
+        }
+    }
+
+    printf("Encerrando sistema...\n");
+    return 0;
+}
